037dabaf936028324485a6b8e10293d4e2d7024d
Frosty-nee <frostbite@frosty-nee.net>
2016-12-22 16:01:15 +0000

    updated jump ranges

--- commands.py
+++ commands.py
@@ -198,7 +198,8 @@ def lightyears(bot, target, nick, command, text):
 		dist += (d1 - d2)**2
 	dist = sqrt(dist) / 9.4605284e15 # meters to lightyears
 	ship_ranges = [
-		('CAP:', 2.5), # jump range for all other ships
+                ('SUPERS:', 3), # jump range for supercarriers and titans
+		('CAP:', 3.5), # jump range for all other ships
 		('BO:', 4.0), # blackops
 		('JF:', 5.0), # jump freighters
 	]
